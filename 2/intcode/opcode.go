a6a8e2ec853982ad4804948656d1fe8d722b1cd8
Kristofer Davidson <kdevb0x@gmail.com>
2019-12-30 14:05:29 -0700

    export opcodes, and ops

--- 2/intcode/opcode.go
+++ 2/intcode/opcode.go
@@ -5,46 +5,46 @@
 package intcode
 
 // opcode in hexidecimal format
-type opcodeHex int
+type OpcodeHex int
 
 const (
-	_      opcodeHex = iota
-	addHex           // 0x01
-	mulHex           // 0x02
+	_      OpcodeHex = iota
+	AddHex           // 0x01
+	MulHex           // 0x02
 
-	hltHex opcodeHex = 0x63 // 99
+	HltHex OpcodeHex = 0x63 // 99
 )
 
 // opcode in decimal format
-type opcode int
+type Opcode int
 
 const (
-	_   opcode = iota
-	add        // 1
-	mul        // 2
-	hlt opcode = 99
+	_   Opcode = iota
+	Add        // 1
+	Mul        // 2
+	Hlt Opcode = 99
 )
 
-var ops = map[opcode]func(...int) int{
-	add: func(oprnd ...int) int {
+var Ops = map[Opcode]func(...int) int{
+	Add: func(oprnd ...int) int {
 		if len(oprnd) != 2 {
 			return 0
 		}
 		return oprnd[0] + oprnd[1]
 	},
-	mul: func(oprnd ...int) int {
+	Mul: func(oprnd ...int) int {
 		if len(oprnd) != 2 {
 			return 0
 		}
 		return oprnd[0] * oprnd[1]
 	},
-	hlt: func(oprnd ...int) int {
-		return int(hlt)
+	Hlt: func(oprnd ...int) int {
+		return int(Hlt)
 	},
 }
 
-func validOpCode(n opcode) bool {
-	if n == add || n == mul || n == hlt {
+func ValidOpCode(n Opcode) bool {
+	if n == Add || n == Mul || n == Hlt {
 		return true
 	}
 	return false
